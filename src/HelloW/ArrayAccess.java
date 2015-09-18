d1377043771aa7f01a61545c25252946bc311a70
pc004 <dogfly1004@naver.com>
2015-09-18 14:42:29 +0900

    JAVA

--- src/HelloW/ArrayAccess.java
+++ src/HelloW/ArrayAccess.java
@@ -7,7 +7,7 @@ public class ArrayAccess {
 		Scanner in= new Scanner(System.in);
 		int intArray[] = new int[5];
 		int max = 0;
-		
+		 
 		for(int i=0; i<5; i++){
 			intArray[i] = in.nextInt();
 			if(intArray[i] > max)
