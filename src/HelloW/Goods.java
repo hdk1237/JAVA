22eba478b1a15b87875be88d359ed37cab6344b6
pc004 <dogfly1004@naver.com>
2015-10-02 15:37:17 +0900

    10.1

---
+++ src/HelloW/Goods.java
@@ -0,0 +1,25 @@
+package HelloW;
+
+public class Goods {
+	String name;
+	int price;
+	int numberOfStock;
+	int sold;
+	
+	
+
+	public static void main(String[] args) {
+		System.out.println("201205040 황동길");
+		Goods camera = new Goods();
+		camera.name="Nikon";
+		camera.price=400000;
+		camera.numberOfStock=30;
+		camera.sold=50;
+		System.out.println("상품 이름: "+camera.name);
+		System.out.println("상품 가격: "+camera.price);
+		System.out.println("재고 수량: "+camera.numberOfStock);
+		System.out.println("팔린 수량: "+camera.sold);
+
+	}
+
+}
